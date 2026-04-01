770856faae74e5a5d966ae7fb11b7ffdd319f8c0
ASSGA <deafdonascimento@gmail.com>
2026-04-01 14:41:05 +0000

    Corrige sys.path para Django Vercel

--- api/index.py
+++ api/index.py
@@ -1,10 +1,10 @@
 import os
 import sys
 
-# Adiciona a raiz do projeto
+# Adiciona a raiz do projeto ao path do Python
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
 
-# Configura Django
+# Configura o Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "associacao.settings")
 
 from django.core.wsgi import get_wsgi_application
