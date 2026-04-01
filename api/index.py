ebc16a45f628fcb17bddee91ee0698ec681c627f
ASSGA <deafdonascimento@gmail.com>
2026-04-01 14:50:19 +0000

    Deploy Django serverless Vercel

--- api/index.py
+++ api/index.py
@@ -1,12 +1,13 @@
 import os
 import sys
 
-# Adiciona a raiz do projeto ao path do Python
+# Adiciona a raiz do projeto ao sys.path
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
 
-# Configura o Django
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "associacao.settings")
+# Configura Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projeto.settings")
 
 from django.core.wsgi import get_wsgi_application
 
+# Cria a aplicação WSGI que o Vercel vai usar
 app = get_wsgi_application()
