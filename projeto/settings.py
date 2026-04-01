89637dbab7dd418b1f940cbc7900d53644fd03f2
ASSGA <deafdonascimento@gmail.com>
2026-04-01 14:23:11 +0000

    Corrige ALLOWED_HOSTS Vercel

--- projeto/settings.py
+++ projeto/settings.py
@@ -14,9 +14,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-r*jt0^_5gcauseda#=g##d%+)y5l$cxwhr85n6tuiypvpyqui_'
 
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    ".vercel.app",
+    "localhost",
+    "127.0.0.1"
+]
 
 
 CSRF_TRUSTED_ORIGINS = [
